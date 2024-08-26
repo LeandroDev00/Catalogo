08d6dee07f5228dc25163a8365308454fb19542c
Leandro Lemos <leandroolemoss17@gmail.com>
2024-08-26 01:04:14 -0300

    Implementado API Cep e lista com imagens do item

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const menu = document.getElementById("menu")
 const cartBtn = document.getElementById("cart-btn")
 const cartModal = document.getElementById("cart-modal")
 const detailModalBtn = document.getElementById("detail-btn")
-const detailsModal = document.getElementById("details-modal")
 const cartItensContainer = document.getElementById("cart-items")
 const cartTotal = document.getElementById("cart-total")
 const Checkout = document.getElementById("checkout-btn")
@@ -10,6 +9,10 @@ const Close = document.getElementById("close-modal-btn")
 const CartCount = document.getElementById("cart-count")
 const AddressInput = document.getElementById("address")
 const AddressWarn = document.getElementById("address-warn")
+
+const AddresRoad = document.getElementById("rua");
+const AddressCity = document.getElementById("cidade");
+const AddressState = document.getElementById("uf");
 
 //Lista para os itens do carrinho
 const ListItem = []
@@ -39,16 +42,18 @@ Close.addEventListener("click", function(){
 menu.addEventListener("click", function(event){
     let parentButton = event.target.closest(".add-to-cart-btn")
     if(parentButton){
+        const codigo = parentButton.getAttribute("data-code")
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
+        const image = parentButton.getAttribute("data-image")
         
-        addCarrinho(name, price);
+        addCarrinho(codigo, name, price, image);
     }
 })
 
 
 //Função addCarrinho
-function addCarrinho(name, price){
+function addCarrinho(codigo, name, price, image){
     const existenItem = ListItem.find(item => item.name === name)
     
     if(existenItem){
@@ -67,8 +72,10 @@ function addCarrinho(name, price){
             },
         }).showToast();
         ListItem.push({
+            codigo,
             name,
             price,
+            image,
             quantity:1
         })
     }
@@ -87,9 +94,12 @@ function UpdateCartModal(){
         let multprice = element.price * element.quantity;
 
         cartItemElement.innerHTML = `
-        <div class="flex items-center justify-between">
+        <div class="flex items-center gap-4 w-full">
             <div>
-                <p class="font-bold">${element.name}</p>
+                <img src="${element.image}" class="w-40 h-20 rounded-md"/>
+            </div>
+            <div class="flex-grow">
+                <p class="font-medium text-start sm:text-ellipsis text-sm">${element.name}</p>
                 <div class="flex items-center border border-gray-500 p-2 rounded-md w-16">
                     <button class="decrement-btn text-black" data-index="${index}">-</button>
                     <p class="quantity mx-2">${element.quantity}</p>
@@ -101,7 +111,7 @@ function UpdateCartModal(){
                 })}</p>
             </div>
             
-            <div>
+            <div class="text-right">
                 <button class="remove-from-cart-btn  text-black" data-name="${element.name}">
                    Remover
                 </button>
@@ -176,6 +186,54 @@ AddressInput.addEventListener("input", function(event){
         AddressWarn.classList.add("hidden");
     }
 })
+function limpa_formulario_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+}
+
+//Inserir CEP
+function pesquisaCep(){
+    const cep = document.getElementById("address").value.replace(/\D/g, "");
+    if(cep != ""){
+        var validaCep = /^[0-9]{8}$/;
+        if(validaCep.test(cep)){
+            //Preenche os campos com "..." enquanto consulta webservice.
+            const rua = document.getElementById('rua').value="...";
+            const bairro = document.getElementById('bairro').value="...";
+            const cidade = document.getElementById('cidade').value="...";
+            document.getElementById('uf').value="...";
+
+            //Cria um elemento javascript
+            fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data =>{
+                if(!data.erro){
+                    document.getElementById('rua').value=data.logradouro;
+                    document.getElementById('bairro').value=data.bairro;
+                    document.getElementById('cidade').value=data.localidade;
+                    document.getElementById('uf').value=data.uf;
+                }
+                else{
+                    limpa_formulario_cep();
+                    alert("CEP não encontrado.");
+                }
+            })
+            .catch(() =>{
+                limpa_formulario_cep();
+                alert("Erro ao consultar CEP.");
+            });
+        } 
+        else{
+            limpa_formulario_cep();
+            alert("Formato de CEP inválido.");
+        }
+    }
+
+    console.log(rua.value);
+}
 
 //Finalizar Pedido
 Checkout.addEventListener("click", function(){
@@ -202,13 +260,12 @@ Checkout.addEventListener("click", function(){
     //Enviar Pedido para api WhatsApp
     const cartItems = ListItem.map((item) =>{
         return (
-            `*${item.name}*\nQuantidade:(${item.quantity})\nPreço:R$${item.price.toFixed(2).replace('.', ',')}\n`
+            `*Cód: ${item.codigo}*\n*${item.name}*\nQuantidade:(${item.quantity})\nPreço:R$${item.price.toFixed(2).replace('.', ',')}\n`
         )
     }).join("\n")
-    
     const totalValue = cartTotal.textContent
 
-    const message = encodeURIComponent(`${cartItems}\nTotal: ${totalValue}\nEndereço: ${AddressInput.value}`)
+    const message = encodeURIComponent(`${cartItems}\nTotal: ${totalValue}\nEndereço: ${rua.value} | ${bairro.value} | ${cidade.value}`)
     const phone = "19989188213"
     
     window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
